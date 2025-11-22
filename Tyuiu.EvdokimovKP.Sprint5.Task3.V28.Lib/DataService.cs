f24fac8dc42a5e0e486ac946eb77577d5881bebe
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-11-22 15:57:12 +0500

    Task3.V28

--- Tyuiu.EvdokimovKP.Sprint5.Task3.V28.Lib/DataService.cs
+++ Tyuiu.EvdokimovKP.Sprint5.Task3.V28.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.EvdokimovKP.Sprint5.Task3.V28.Lib
         {
             string combine = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
 
-            double y = -1 / 4 * (Math.Pow(x, 3) - (3 * (Math.Pow(x, 2)) + 4));
+            double y = -0.25 * (Math.Pow(x, 3) - (3 * (Math.Pow(x, 2)) + 4));
 
             y = Math.Round(y, 2);
 
