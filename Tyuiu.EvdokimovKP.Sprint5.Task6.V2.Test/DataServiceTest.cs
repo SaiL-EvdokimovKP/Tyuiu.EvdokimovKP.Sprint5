83b847b33f5761e1f282219b8ea504fe689cf5c7
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-11-24 00:58:02 +0500

    Task6.V2

---
+++ Tyuiu.EvdokimovKP.Sprint5.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.EvdokimovKP.Sprint5.Task6.V2.Lib;
+using System.IO;
+
+namespace Tyuiu.EvdokimovKP.Sprint5.Task6.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\evdok\AppData\Local\Temp\InPutDataFileTask6V2.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
